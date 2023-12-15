78509da5376a72b1bfa342665bd887435a40fd88
XavierBusca <novasmurf05@gmail.com>
2023-12-15 10:41:24 +0100

    comprovem si va la equacion

--- Estructures.py
+++ Estructures.py
@@ -4,3 +4,6 @@ Edat= (input("Quina es la teva edad? "))
 Nota1= int(input("Quina es la teva primera nota? "))
 Nota2= int(input("Quina es la teva segona nota? "))
 Nota3= int(input("Quina es la teva tercera nota? "))
+
+NotaFinal1 = (Nota1 *10)/30
+print(NotaFinal1)
