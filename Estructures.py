068535e99805ab012ac9fdf036a289af8ab445aa
XavierBusca <novasmurf05@gmail.com>
2023-12-15 11:59:12 +0100

    Fem el if sencer y en contes de tuples fem llistas

--- Estructures.py
+++ Estructures.py
@@ -6,6 +6,8 @@ Nota2= int(input("Quina es la teva segona nota? "))
 Nota3= int(input("Quina es la teva tercera nota? "))
 
 alumno= (Nom, Cognom, Edat, Nota1, Nota2, Nota3)
+Superior_A_7=[]
+Superior_A_8=[]
 
 NotaFinal1 = (Nota1 *0.3)
 NotaFinal2 = (Nota2 * 0.4)
@@ -14,4 +16,6 @@ NotaFinal3 = (Nota3 * 0.4)
 NotaFinal = NotaFinal1 + NotaFinal2 + NotaFinal3
 
 if NotaFinal > 7:
-    print(alumno)
+    Superior_A_7.append(alumno)
+    if Nota1 or Nota2 or Nota3 > 8:
+        Superior_A_8.append(alumno)
