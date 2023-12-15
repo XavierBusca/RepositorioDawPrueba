abb30d546744f8fdc50be000e81c523bc3fe9986
XavierBusca <novasmurf05@gmail.com>
2023-12-15 11:55:20 +0100

    arreglem el if

--- Estructures.py
+++ Estructures.py
@@ -5,13 +5,13 @@ Nota1= int(input("Quina es la teva primera nota? "))
 Nota2= int(input("Quina es la teva segona nota? "))
 Nota3= int(input("Quina es la teva tercera nota? "))
 
-Tupla1= (Nom,Cognom, Edat, Nota1, Nota2, Nota3)
+alumno= (Nom, Cognom, Edat, Nota1, Nota2, Nota3)
 
-NotaFinal1 = (Nota1 *10)/30
-NotaFinal2 = (Nota2 * 10)/40
-NotaFinal3 = (Nota3 * 10)/30
+NotaFinal1 = (Nota1 *0.3)
+NotaFinal2 = (Nota2 * 0.4)
+NotaFinal3 = (Nota3 * 0.4)
 
 NotaFinal = NotaFinal1 + NotaFinal2 + NotaFinal3
 
-if NotaFinal < 7 and NotaFinal1,NotaFinal2,NotaFinal3 < 8:
-    print(Tupla1)
+if NotaFinal > 7:
+    print(alumno)
