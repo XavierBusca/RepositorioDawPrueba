4d6e1c0cab0ef68e5af54056999cd7bbd777c71d
XavierBusca <novasmurf05@gmail.com>
2023-12-19 12:12:03 +0100

    Ara hem fet que el programa faci com a maxim 3 alumne

--- Estructures.py
+++ Estructures.py
@@ -1,24 +1,31 @@
-Nom= (input("Quin es el teu nom? "))
-Cognom= (input("Quin es el teu cognom? "))
-Edat= (input("Quina es la teva edad? "))
-Nota1= int(input("Quina es la teva primera nota? "))
-Nota2= int(input("Quina es la teva segona nota? "))
-Nota3= int(input("Quina es la teva tercera nota? "))
-
-alumno= (Nom, Cognom, Edat, Nota1, Nota2, Nota3)
+contador= 1
+NumAlum2=4
+   
 Superior_A_7=[]
 Superior_A_8=[]
 
-NotaFinal1 = (Nota1 *0.3)
-NotaFinal2 = (Nota2 * 0.4)
-NotaFinal3 = (Nota3 * 0.4)
+while contador < NumAlum2:
+    Nom= (input("Quin es el teu nom? "))
+    Cognom= (input("Quin es el teu cognom? "))
+    Edat= (input("Quina es la teva edad? "))
+    Nota1= int(input("Quina es la teva primera nota? "))
+    Nota2= int(input("Quina es la teva segona nota? "))
+    Nota3= int(input("Quina es la teva tercera nota? "))
 
-NotaFinal = NotaFinal1 + NotaFinal2 + NotaFinal3
+    alumno= (Nom, Cognom, Edat, Nota1, Nota2, Nota3)
 
-if NotaFinal > 7:
-    Superior_A_7.append(alumno)
-    if Nota1 or Nota2 or Nota3 >= 8:
-        Superior_A_8.append(alumno)
+    NotaFinal1 = (Nota1 *0.3)
+    NotaFinal2 = (Nota2 * 0.4)
+    NotaFinal3 = (Nota3 * 0.4)
+
+    NotaFinal = NotaFinal1 + NotaFinal2 + NotaFinal3
+
+    if NotaFinal > 7:
+        Superior_A_7.append(alumno)
+        if Nota1 or Nota2 or Nota3 >= 8:
+            Superior_A_8.append(alumno)
+    
+    contador = contador + 1
 
 print("La nota media es igual o mayor a un 7:")
 for alumno in Superior_A_7:
